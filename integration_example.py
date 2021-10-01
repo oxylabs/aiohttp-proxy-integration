891690123b6967a7e153c66beffa01dfb9615fe2
oxyjonas <87811765+oxyjonas@users.noreply.github.com>
2021-10-01 15:45:11 +0300

    Add files via upload

--- integration_example.py
+++ integration_example.py
@@ -1,6 +1,6 @@
-import aiohttp
-import asyncio
 import sys 
+
+import aiohttp
 
 USER = "user"
 PASSWORD = "password"
@@ -10,17 +10,24 @@ END_POINT = "pr.oxylabs.io:7777"
 # async def fetch():
 #     async with aiohttp.ClientSession() as session:
 #         proxy_auth = aiohttp.BasicAuth(USER, PASS)
-#         async with session.get("http://ip.oxylabs.io", proxy="http://pr.oxylabs.io:7777", proxy_auth=proxy_auth) as resp:
+#         async with session.get("http://ip.oxylabs.io", 
+#             proxy="http://pr.oxylabs.io:7777", 
+#             proxy_auth=proxy_auth 
+#         ) as resp:
 #             print(await resp.text())
+import asyncio
 
 # Passing authentication credentials in proxy URL:
+ 
 async def fetch():
-	async with aiohttp.ClientSession() as session:
-		async with session.get('http://ip.oxylabs.io', proxy=f"http://{USER}:{PASSWORD}@{END_POINT}") as response: 
-			print(await response.text())
+    async with aiohttp.ClientSession() as session:
+        async with session.get("http://ip.oxylabs.io", 
+            proxy=f"http://{USER}:{PASSWORD}@{END_POINT}"
+        ) as resp: 
+            print(await resp.text())
 
 if __name__ == "__main__":
-	# Alternative event loop if you're running code on Windows OS 
-	if sys.platform.startswith("win") and sys.version_info.minor >= 8:
-		asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-	asyncio.run(fetch())
+    # Alternative event loop if you're running code on Windows OS 
+    if sys.platform.startswith("win") and sys.version_info.minor >= 8:
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(fetch())
--- sample_project.py
+++ sample_project.py
@@ -1,49 +1,64 @@
-import aiohttp
 import asyncio
+import time
+import sys
+import os
+
 from bs4 import BeautifulSoup
 import pandas as pd
-import sys
-import time
+import aiohttp
 
 USER = "user"
 PASSWORD = "pass"
 END_POINT = "pr.oxylabs.io:7777"
 
 # Generate a list of URLs to scrape
-url_list = [f"https://books.toscrape.com/catalogue/category/books_1/page-{page_num}.html" for page_num in range(1, 51)]
+url_list = [
+f"https://books.toscrape.com/catalogue/category/books_1/page-{page_num}.html" 
+for page_num 
+in range(1, 51)
+]
 
 async def fetch(session, sem, url):
-	async with sem:
-		async with session.get(url, proxy=f"http://{USER}:{PASSWORD}@{END_POINT}") as response: 
-			await parse_url(await response.text())
+        async with sem:
+            async with session.get(url, 
+                proxy=f"http://{USER}:{PASSWORD}@{END_POINT}"
+            ) as response:
+                await parse_url(await response.text())
 
 async def parse_url(text):
-	soup = BeautifulSoup(text, "lxml")
-	for product_data in soup.select("ol.row > li > article.product_pod"):
-		data = {
-		"Title": product_data.select_one("h3 > a")["title"],
-		"URL": product_data.select_one("h3 > a").get("href")[5:],
-		"Product Price": product_data.select_one("p.price_color").text,
-		"Stars": product_data.select_one("p")["class"][1],
-		}
-		full_data.append(data)
-		print(f"Grabbing book: {data['Title']}")
-	
+    soup = BeautifulSoup(text, "lxml")
+    for product_data in soup.select("ol.row > li > article.product_pod"):
+        data = {
+            "title": product_data.select_one("h3 > a")["title"],
+            "url": product_data.select_one("h3 > a").get("href")[5:],
+            "product_price": product_data.select_one("p.price_color").text,
+            "stars": product_data.select_one("p")["class"][1],
+        }
+        final_list.append(data)
+        print(f"Grabing book: {data['title']}")
+    
 async def create_jobs():
-	sem = asyncio.Semaphore(4)
-	async with aiohttp.ClientSession() as session:
-		get_results = await asyncio.gather(*[fetch(session, sem, url) for url in url_list])
-
+    final_res = []
+    sem = asyncio.Semaphore(4)
+    async with aiohttp.ClientSession() as session:
+        await asyncio.gather(*[fetch(session, sem, url) 
+        for url in url_list
+        ])
+        
 if __name__ == "__main__":
-	full_data = []
-	try: 
-		start = time.time()
-		# Different Event Loop Policy must be loaded if you're using Windows OS
-		if sys.platform.startswith("win") and sys.version_info.minor >= 8:
-			asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-		asyncio.run(create_jobs())
-	finally:
-		print(f"Total of {len(full_data)} products gathered in {time.time() - start} seconds")
-		df = pd.DataFrame(full_data)
-		df["URL"] = df["URL"].map(lambda x: ''.join(["https://books.toscrape.com/catalogue", x]))
-		df.to_csv("scraped-books.csv", encoding='utf-8-sig', index=False)
+    final_list = []
+    start = time.perf_counter()
+    # Different Event Loop Policy must be loaded if you're using Windows OS to avoid "Event Loop is closed"
+    if sys.platform.startswith("win") and sys.version_info.minor >= 8:
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    try:
+      asyncio.run(create_jobs())   
+    except Exception: 
+        print("We broke, but there might still be some results")
+    
+    print(f"\nTotal of {len(final_list)} products gathered in {time.perf_counter() - start:.2f} seconds")
+    df = pd.DataFrame(final_list)
+    df["url"] = df["url"].map(lambda x: ''.join(["https://books.toscrape.com/catalogue", x]))
+    filename = "scraped-books.csv"
+    df.to_csv(filename, encoding='utf-8-sig', index=False)
+    print(f"\nExtracted data can be accessed at {os.path.join(os.getcwd(), filename)}")
